f99e90fa5a555e78b2d8c5288618d26176e03d8c
ydro <dubstabber>
2023-12-25 19:03:37 +0100

    Add key handling and locked doors

---
+++ scenes/globals/preloads.gd
@@ -0,0 +1,8 @@
+extends Node
+
+var ruby_key := preload("res://images/items/REDKA0.png")
+var weird_key := preload("res://images/items/WEIRA0.png")
+var brown_key := preload("res://images/items/BROWA0.png")
+var gold_key := preload("res://images/items/YKGOA0.png")
+var emerald_key := preload("res://images/items/EMERA0.png")
+var silver_key := preload("res://images/items/SILVA0.png")
--- scenes/objects/Door.gd
+++ scenes/objects/Door.gd
@@ -10,23 +10,30 @@ var closed_position := position.y
 var opened_position: float
 var is_opening := false
 var tween: Tween
-
+var map: Node3D
 
 func _ready():
 	opened_position = closed_position + move_range
-	
+	map = get_tree().get_first_node_in_group('map')
 
 func open():
-	is_opening = not is_opening
-	if tween and tween.is_running() and not open_only:
-		tween.stop()
-	if is_opening:
-		tween = create_tween()
-		await tween.tween_property(self, "position:y", opened_position, move_speed).finished
-		if not open_only:
-			await get_tree().create_timer(time_to_close).timeout
-			open()
-	elif not is_opening and not open_only:
-		tween = create_tween()
-		await tween.tween_property(self, "position:y", closed_position, move_speed).finished
+	var isUnlocked = true
+	if key_needed and not key_needed in map.keys_collected:
+		isUnlocked = false
+	
+	if isUnlocked:
+		is_opening = not is_opening
+		if tween and tween.is_running() and not open_only:
+			tween.stop()
+		if is_opening:
+			tween = create_tween()
+			await tween.tween_property(self, "position:y", opened_position, move_speed).finished
+			if not open_only:
+				await get_tree().create_timer(time_to_close).timeout
+				open()
+		elif not is_opening and not open_only:
+			tween = create_tween()
+			await tween.tween_property(self, "position:y", closed_position, move_speed).finished
+	else:
+		print("You need the "+key_needed+" key!")
 

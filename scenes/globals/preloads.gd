96f23b54502511de6532065658d43e9090f9148c
ydro <dubstabber>
2023-12-26 22:26:09 +0100

    Add some sfx, new assets, fixes

--- scenes/globals/preloads.gd
+++ scenes/globals/preloads.gd
@@ -6,3 +6,12 @@ var brown_key := preload("res://images/items/BROWA0.png")
 var gold_key := preload("res://images/items/YKGOA0.png")
 var emerald_key := preload("res://images/items/EMERA0.png")
 var silver_key := preload("res://images/items/SILVA0.png")
+
+
+var door_locked_sound := preload("res://sounds/sfx/DOORLOCK.ogg")
+var key_collected_sound := preload("res://sounds/sfx/DSKEYPIC.wav")
+
+var open_door_sound := preload("res://sounds/sfx/DSDOROPN.ogg")
+var close_door_sound := preload("res://sounds/sfx/DSDORCLS.ogg")
+
+var kill_player_sound := preload("res://sounds/sfx/DSSLOP.wav")

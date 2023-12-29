3e31b61ba7329644d602cd3a8c0611e5091cba69
ydro <dubstabber>
2023-12-29 18:04:56 +0100

    Rework player spawning, camera handling moved to events' scripts

--- scenes/objects/AreaEvent.gd
+++ scenes/objects/AreaEvent.gd
@@ -5,6 +5,7 @@ signal event_triggered(event_name)
 @export var one_trigger_only := true
 @export var event_name: String
 @export var door_to_open: Node3D
+@export var temporary_camera: Camera3D
 
 var triggered := false
 
@@ -15,9 +16,8 @@ func _ready():
 
 func _body_entered(body):
 	if body.is_in_group('player') and not triggered:
-		if one_trigger_only:
-			triggered = true
-		if door_to_open:
-			door_to_open.open()
+		if one_trigger_only: triggered = true
+		if temporary_camera: temporary_camera.set_current(true)
+		if door_to_open: door_to_open.open()
 		event_triggered.emit(event_name)
 

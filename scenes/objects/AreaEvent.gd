e597f76775a3d065368b437ac8bd012afaef7dea
ydro <dubstabber>
2023-12-29 17:16:00 +0100

    Add area events and handling them

---
+++ scenes/objects/AreaEvent.gd
@@ -0,0 +1,23 @@
+extends Area3D
+
+signal event_triggered(event_name)
+
+@export var one_trigger_only := true
+@export var event_name: String
+@export var door_to_open: Node3D
+
+var triggered := false
+
+
+func _ready():
+	connect("body_entered", _body_entered)
+
+
+func _body_entered(body):
+	if body.is_in_group('player') and not triggered:
+		if one_trigger_only:
+			triggered = true
+		if door_to_open:
+			door_to_open.open()
+		event_triggered.emit(event_name)
+

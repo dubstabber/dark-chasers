c897e677a34e90ba4a33beadb5bcce91c439b6a1
ydro <dubstabber>
2024-01-25 16:06:16 +0100

    Make enemy abstract scene, some fixes and adjustments, new debug function

--- scenes/enemies/enemy.gd
+++ scenes/enemies/enemy.gd
@@ -1,57 +1,115 @@
-extends CharacterBody3D
+class_name Enemy extends CharacterBody3D
 
-const SPEED = 7.0
-const ACCEL = 10
-
-@export var specific_enemy: String
+@export var current_room: String
+@export var disappear_zones: Array[Area3D]
+@export var is_wandering: bool = false
 
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
-var target: CharacterBody3D
-var enemy_data: Dictionary
-var direction: Vector3
+var players: Node3D
+var current_target: CharacterBody3D
+var waypoints: Array
 var jump_speed: float = 0
+var direction: Vector3
+var map_transitions: Node3D
+var ground_type: String
 
-@onready var nav := $NavigationAgent3D
-@onready var image := $Sprite3D
-@onready var find_path_timer := $FindPathTimer
-@onready var sound_music := $SoundMusic
+var speed: float
+var accel: float
+
+@onready var nav = $NavigationAgent3D
+@onready var find_path_timer = $Timers/FindPathTimer
+@onready var wandering_timer = $Timers/WanderingTimer
+@onready var rotation_controller = $RotationController
+@onready var interaction_ray = $RotationController/Interaction
 
 
 func _ready():
-	target = get_tree().get_first_node_in_group("player")
-	if specific_enemy:
-		for enemy in EnemyDb.ENEMIES:
-			if enemy.name == specific_enemy:
-				enemy_data = enemy
-				image.texture = enemy_data.image
-				break
-	if not enemy_data:
-		enemy_data = EnemyDb.ENEMIES.pick_random()
-		image.texture = enemy_data.image
-	var sizeto = 300
-	var size = image.texture.get_size()
-	if size.x > sizeto or size.y > sizeto or size.x <= sizeto or size.y <= sizeto:
-		var sizeIt = size.x if size.x > size.y else size.y
-		var scalefactor = sizeto / sizeIt
-		image.scale = Vector3(scalefactor, scalefactor, 1)
-	if "music" in enemy_data:
-		sound_music.stream = enemy_data.music
-		sound_music.play()
-	elif "musics" in enemy_data:
-		sound_music.connect("finished", _draw_music)
-		_draw_music()
+	players = get_tree().get_first_node_in_group("players")
+	map_transitions = get_tree().get_first_node_in_group("transitions")
+	for disappear_zone in disappear_zones:
+		disappear_zone.connect("body_entered", _on_disappear_area)
 
 
 func _physics_process(delta):
-	if target:
-		direction = (nav.get_next_path_position() - global_position).normalized()
-		velocity = velocity.lerp(direction * (SPEED + jump_speed), ACCEL * delta)
-		move_and_slide()
+	if not is_on_floor():
+		velocity.y -= gravity * delta
+	if not current_target:
+		check_targets()
+	if current_target or waypoints:
+		var distance_to_target = nav.distance_to_target()
+		if distance_to_target < 0.2:
+			velocity = Vector3.ZERO
+		else:
+			var next_pos = nav.get_next_path_position()
+			if global_position != next_pos and is_on_floor():
+				rotation_controller.look_at(next_pos)
+			direction = (next_pos - global_position).normalized()
+			velocity = velocity.lerp(direction * (speed + jump_speed), accel * delta)
+	elif is_wandering:
+		if wandering_timer.is_stopped():
+			wandering_timer.start()
+		velocity = velocity.lerp(direction * (speed + jump_speed), accel * delta)
+	move_and_slide()
+
+
+func check_targets():
+	if players:
+		var space_state = get_world_3d().direct_space_state
+		for target in players.get_children():
+			var params = PhysicsRayQueryParameters3D.new()
+			params.from = global_position
+			params.to = target.camera_3d.global_position
+			params.exclude = [self]
+			params.collision_mask = collision_mask
+			var result = space_state.intersect_ray(params)
+			if (
+				result
+				and result.collider.is_in_group("player")
+				and not target.killed
+			):
+				current_target = result.collider
 
 
 func makepath() -> void:
-	if !!target:
-		nav.target_position = target.global_position
+	if current_target:
+		if current_target.current_room == current_room or not current_room:
+			nav.target_position = current_target.global_position
+		elif map_transitions:
+			var transition_point = find_path_to_player()[0]
+			nav.target_position = map_transitions.get_node(transition_point).global_position
+	elif waypoints:
+		nav.target_position = waypoints[0]
+
+
+func find_path_to_player():
+	var visitedRooms = []
+	var queue = [[current_room]]
+
+	while queue:
+		var path = queue.pop_front()
+		var c_room = path[-1]
+
+		if c_room == current_target.current_room:
+			var transitions = []
+			for i in range(1, path.size(), 2):
+				transitions.append(path[i])
+			return transitions
+		if c_room not in visitedRooms:
+			visitedRooms.append(c_room)
+			for transitionPoint in map_transitions.map_transitions[c_room].keys():
+				var nextRoom = map_transitions.map_transitions[c_room][transitionPoint]
+				if transitionPoint == "ThirdFloorAbyss":
+					continue
+				if nextRoom not in visitedRooms:
+					var new_path = path.duplicate()
+					new_path.append(transitionPoint)
+					new_path.append(nextRoom)
+					queue.append(new_path)
+	return null
+
+
+func add_disappear_zone(area):
+	area.connect("body_entered", _on_disappear_area)
 
 
 func _on_find_path_timer_timeout():
@@ -59,34 +117,57 @@ func _on_find_path_timer_timeout():
 	if distance_to_target < 10:
 		find_path_timer.wait_time = 0.1
 	elif distance_to_target < 20:
-		find_path_timer.wait_time = 0.3
+		find_path_timer.wait_time = 0.2
 	elif distance_to_target < 35:
-		find_path_timer.wait_time = 0.6
+		find_path_timer.wait_time = 0.5
 	elif distance_to_target < 50:
-		find_path_timer.wait_time = 1.0
+		find_path_timer.wait_time = 0.8
 	else:
-		find_path_timer.wait_time = 2.0
+		find_path_timer.wait_time = 1.7
 	makepath()
 
 
-func _draw_music():
-	sound_music.stream = enemy_data.musics.pick_random()
-	sound_music.play()
-
-
-func _on_area_3d_body_entered(body):
+func _on_kill_zone_body_entered(body):
 	if body.is_in_group("player"):
 		body.kill(position)
-		target = null
+		current_target = null
+		velocity = Vector3.ZERO
+
+
+func _on_interaction_timer_timeout():
+	var collider = interaction_ray.get_collider()
+	if collider:
+		var parent = collider.get_parent()
+		if parent.is_in_group("door") and parent.can_manual_open:
+			parent.open(collider.name)
+		elif is_wandering:
+			direction = Vector3(-direction.x, 0, -direction.z)
+			if global_position != global_position + direction and is_on_floor():
+				rotation_controller.look_at(global_position + direction)
+
+
+func _on_wandering_timer_timeout():
+	wandering_timer.wait_time = randf_range(0.5, 3)
+	direction = Vector3(randf_range(-1, 1), 0, randf_range(-1, 1)).normalized()
+	if global_position != global_position + direction and is_on_floor():
+		rotation_controller.look_at(global_position + direction)
+
+
+func _on_navigation_agent_3d_target_reached():
+	if waypoints:
+		waypoints.pop_back()
+		velocity = Vector3.ZERO
 
 
 func _on_navigation_agent_3d_link_reached(details):
-	if details.owner.is_in_group("jump-up"):
-		velocity.y = 12
-		jump_speed = gravity
 	if details.owner.is_in_group("jump-down"):
 		jump_speed = gravity
 
 
 func _on_navigation_agent_3d_waypoint_reached(_details):
 	jump_speed = 0
+
+func _on_disappear_area(body):
+	if body == self:
+		queue_free()
+

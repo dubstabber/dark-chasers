7b2e17fb7e8bf921eee012e8021b7ee540929f66
ydro <dubstabber>
2024-02-06 17:34:52 +0100

    Add custom font handling

--- scenes/hud.gd
+++ scenes/hud.gd
@@ -21,17 +21,17 @@ func fade_black_screen():
 
 
 func show_event_text(text: String):
-	if event_label.text:
+	if event_label.get_child_count():
 		tween = create_tween()
-		await tween.tween_property(event_label, "theme_override_colors/font_color:a", 0, 1.0).finished
-	event_label.text = text
+		await tween.tween_property(event_label, "modulate:a", 0, 1.0).finished
+	event_label.set_text_with_custom_font(text)
 	tween = create_tween()
-	await tween.tween_property(event_label, "theme_override_colors/font_color:a", 1, 0.4).finished
+	await tween.tween_property(event_label, "modulate:a", 1, 0.4).finished
 
 
 func hide_event_text():
 	tween = create_tween()
-	tween.tween_property(event_label, "theme_override_colors/font_color:a", 0, 1.0)
+	tween.tween_property(event_label, "modulate:a", 0, 1.0)
 
 
 func _on_player_mode_changed(mode, value):

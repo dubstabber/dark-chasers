675af23ae5be6ec38b55d9c50f089dde5cd4af6e
ydro <dubstabber>
2024-02-20 09:02:56 +0100

    Reimplement custom fonts, fix labels and hud

--- scenes/ui/log_label.gd
+++ scenes/ui/log_label.gd
@@ -1,10 +1,17 @@
-extends Label
+extends Control
 
+@onready var log_text = $AoOniFont
+
+
+func _ready():
+	log_text.set_font_scale(0.3)
+	custom_minimum_size = Vector2(500,10)
 
 func create(log_msg: String, wait_time: float):
-	text = log_msg
+	log_text.set_text_with_aooni_font(log_msg)
 	get_tree().create_timer(wait_time).connect("timeout", fade_out)
-	
+
+
 func fade_out():
 	await create_tween().tween_property(self, "modulate:a", 0, 1.0).finished
 	queue_free()

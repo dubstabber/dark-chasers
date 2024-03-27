afa390717cc98390f827ef11c61a20adf0849810
ydro <dubstabber>
2024-03-27 18:46:50 +0100

    New test map, add scraps to pot flower

--- scenes/objects/flower_pot.gd
+++ scenes/objects/flower_pot.gd
@@ -22,4 +22,22 @@ func take_damage(dmg: int):
 				pot_scrap.set_scrap_type("pot scrap")
 				pot_scrap.position = global_position
 				pot_scrap.linear_velocity = Vector3(randf_range(-4,4),5,randf_range(-4,4))
-				
+			for i in [6,10].pick_random():
+				var circle_scrap = Preloads.SCRAP_SCENE.instantiate()
+				get_parent().add_child(circle_scrap)
+				circle_scrap.set_scrap_type("circle ground scrap")
+				circle_scrap.position = global_position
+				circle_scrap.linear_velocity = Vector3(randf_range(-2,2),3.5,randf_range(-2,2))
+			for i in [4,6].pick_random():
+				var small_scrap = Preloads.SCRAP_SCENE.instantiate()
+				get_parent().add_child(small_scrap)
+				small_scrap.set_scrap_type("small ground scrap")
+				small_scrap.position = global_position
+				small_scrap.linear_velocity = Vector3(randf_range(-2,2),3.5,randf_range(-2,2))
+			for i in 10:
+				var grass_scrap = Preloads.SCRAP_SCENE.instantiate()
+				get_parent().add_child(grass_scrap)
+				grass_scrap.set_scrap_type("grass scrap")
+				grass_scrap.position = global_position
+				grass_scrap.position.y -= 0.1
+				grass_scrap.linear_velocity = Vector3(randf_range(-3,3),1,randf_range(-3,3))

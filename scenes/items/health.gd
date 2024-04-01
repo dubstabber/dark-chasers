685cc735b4a25df9ad42ab040caad100aa047863
ydro <dubstabber>
2024-04-01 16:55:28 +0200

    Refactor hud, items receive log signal

--- scenes/items/health.gd
+++ scenes/items/health.gd
@@ -1,5 +1,7 @@
 extends Area3D
 
+signal item_pickedup(event_string)
+
 @export var event_string: String
 @export var pickup_sound: AudioStream
 @export var heal_value := 10
@@ -8,5 +10,5 @@ func _on_body_entered(body):
 	if body.is_in_group('player'):
 		if pickup_sound: 
 			Utils.play_sound(pickup_sound, get_parent(), position)
-		print(event_string)
+		item_pickedup.emit(event_string)
 		queue_free()

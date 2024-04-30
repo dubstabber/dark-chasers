d5fdfdf3b1cb524d1f46b26905ab5bc56c959120
dubstabber <taesan@protonmail.com>
2024-04-30 17:37:07 +0200

    Add new levels, improve code, some fixes and cleanup

--- scenes/enemies/ao_oni.gd
+++ scenes/enemies/ao_oni.gd
@@ -17,18 +17,20 @@ func _physics_process(delta):
 
 
 func animate_sprite():
-	var p_pos = rotation_controller.global_position.direction_to(get_viewport().get_camera_3d().global_position)
-	var vertical_side = rotation_controller.global_transform.basis.z
-	var horizontal_side = rotation_controller.global_transform.basis.x
-	var h_dot = horizontal_side.dot(p_pos)
-	var v_dot = vertical_side.dot(p_pos)
-	var state = "run" if velocity else "stay"
-	if v_dot < -0.5 and not animation_player.current_animation.contains(state+'-front'):
-		animation_player.play(state + "-front")
-	elif v_dot > 0.5 and not animation_player.current_animation.contains(state+'-back'):
-		animation_player.play(state + "-back")
-	else:
-		sprite_3d.flip_h = h_dot > 0
-		if abs(v_dot) < 0.3 and not animation_player.current_animation.contains(state+'-side'):
-			animation_player.play(state + "-side")
+	var current_camera = get_viewport().get_camera_3d()
+	if current_camera:
+		var p_pos = rotation_controller.global_position.direction_to(current_camera.global_position)
+		var vertical_side = rotation_controller.global_transform.basis.z
+		var horizontal_side = rotation_controller.global_transform.basis.x
+		var h_dot = horizontal_side.dot(p_pos)
+		var v_dot = vertical_side.dot(p_pos)
+		var state = "run" if velocity else "stay"
+		if v_dot < -0.5 and not animation_player.current_animation.contains(state+'-front'):
+			animation_player.play(state + "-front")
+		elif v_dot > 0.5 and not animation_player.current_animation.contains(state+'-back'):
+			animation_player.play(state + "-back")
+		else:
+			sprite_3d.flip_h = h_dot > 0
+			if abs(v_dot) < 0.3 and not animation_player.current_animation.contains(state+'-side'):
+				animation_player.play(state + "-side")
 

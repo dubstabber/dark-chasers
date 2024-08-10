62d574b18bdb97223e22fbdaa2fdc31ac613e244
dubstabber <taesan@protonmail.com>
2024-08-10 18:11:43 +0200

    Add some grass, fix nav region part on map, improve rotation for enemy

--- scenes/enemies/ao_mika.gd
+++ scenes/enemies/ao_mika.gd
@@ -1,7 +1,7 @@
 extends Enemy
 
-@onready var animation_player = $RotationController/AnimationPlayer
-@onready var sprite_3d = $RotationController/Sprite3D
+@onready var animation_player = $Graphics/AnimationPlayer
+@onready var sprite_3d = $Graphics/Sprite3D
 
 
 func _ready():
@@ -17,17 +17,19 @@ func _physics_process(delta):
 
 
 func animate_sprite():
-	var p_pos = rotation_controller.global_position.direction_to(get_viewport().get_camera_3d().global_position)
-	var vertical_side = rotation_controller.global_transform.basis.z
-	var horizontal_side = rotation_controller.global_transform.basis.x
-	var h_dot = horizontal_side.dot(p_pos)
-	var v_dot = vertical_side.dot(p_pos)
-	var state = "run" if velocity else "stay"
-	if v_dot < -0.5 and not animation_player.current_animation.contains(state+'-front'):
-		animation_player.play(state + "-front")
-	elif v_dot > 0.5 and not animation_player.current_animation.contains(state+'-back'):
-		animation_player.play(state + "-back")
-	else:
-		sprite_3d.flip_h = h_dot > 0
-		if abs(v_dot) < 0.3 and not animation_player.current_animation.contains(state+'-side'):
-			animation_player.play(state + "-side")
+	var current_camera = get_viewport().get_camera_3d()
+	if current_camera:
+		var p_pos = graphics.global_position.direction_to(current_camera.global_position)
+		var vertical_side = graphics.global_transform.basis.z
+		var horizontal_side = graphics.global_transform.basis.x
+		var h_dot = horizontal_side.dot(p_pos)
+		var v_dot = vertical_side.dot(p_pos)
+		var state = "run" if velocity else "stay"
+		if v_dot < -0.5 and not animation_player.current_animation.contains(state+'-front'):
+			animation_player.play(state + "-front")
+		elif v_dot > 0.5 and not animation_player.current_animation.contains(state+'-back'):
+			animation_player.play(state + "-back")
+		else:
+			sprite_3d.flip_h = h_dot > 0
+			if abs(v_dot) < 0.3 and not animation_player.current_animation.contains(state+'-side'):
+				animation_player.play(state + "-side")

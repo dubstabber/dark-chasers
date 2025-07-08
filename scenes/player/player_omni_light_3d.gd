daba0df458dd7ee6a9ab806eec8b4fc329e80d56
dubstabber <taesan@protonmail.com>
2025-07-08 19:49:43 +0200

    Implement lighting light when lighter is 'firing', animation tine tuning

---
+++ scenes/player/player_omni_light_3d.gd
@@ -0,0 +1,19 @@
+extends OmniLight3D
+
+
+@export var lighter_source: Node
+
+
+func _ready() -> void:
+	lighter_source.lighter_on.connect(light_lighter)
+	lighter_source.lighter_off.connect(extinguish_lighter)
+
+
+func light_lighter() -> void:
+	light_energy = 0.6
+	light_color = Color.YELLOW
+
+
+func extinguish_lighter() -> void:
+	light_energy = 0.06
+	light_color = Color.WHITE

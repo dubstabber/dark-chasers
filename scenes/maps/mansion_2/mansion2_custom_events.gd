5d508cd1f75b74f53132e093b8c3fa807fba7ad1
dubstabber <taesan@protonmail.com>
2026-03-27 15:14:02 +0100

    Enhance mansion2 scene with new enemy interactions and events
    - Added new enemy references for Blueberry Farm, including Fuwatty, Jellyfish Oni, Hundred Eyes Oni, Squatto, and Hunchback Oni.
    - Implemented custom events for managing enemy behaviors and interactions in the Blueberry Farm area.
    - Updated mansion2_custom_events.gd to handle new chase sequences and enemy disappearance zones.
    - Incremented load steps in mansion2 scene to reflect recent changes and additions.

--- scenes/maps/mansion_2/mansion2_custom_events.gd
+++ scenes/maps/mansion_2/mansion2_custom_events.gd
@@ -4,18 +4,42 @@ extends Node
 @export var ao_oni2: Enemy
 @export var fuwatty: Enemy
 
+@export var blueberry_farm_fuwatty: Enemy
+@export var blueberry_farm_jellyfish: Enemy
+@export var blueberry_farm_hundred_eyed_monster: Enemy
+@export var blueberry_farm_squatto: Enemy
+@export var blueberry_farm_hunchback_monster: Enemy
+
+@export var blueberry_door: Door
+@export var blueberry_farm_blocker: StaticBody3D
+
+@export var blueberry_farm_open_door_zone: Area3D
+
+@export var blueberry_farm_all_enemies_disappear_zone: Area3D
+
 var _ao_oni1_triggered := false
 var _ao_oni2_triggered := false
 var _fuwatty_triggered := false
+var _blueberry_farm_open_door_zone_triggered := false
+var _blueberry_farm_all_enemies_disappear_zone_triggered := false
 
 
 func _ready() -> void:
 	Services.event_bus.subscribe(GameEventTypes.ENEMY_TARGET_ACQUIRED, _on_aooni_target_acquired)
-	
+	Services.event_bus.subscribe(GameEventTypes.CUSTOM_BLUEBERRY_FARM_CHASE_SEQUENCE, _on_blueberry_farm_chase_sequence)
+	if blueberry_farm_open_door_zone:
+		blueberry_farm_open_door_zone.body_entered.connect(_on_blueberry_farm_open_door_zone_body_entered)
+	if blueberry_farm_all_enemies_disappear_zone:
+		blueberry_farm_all_enemies_disappear_zone.body_entered.connect(_on_blueberry_farm_all_enemies_disappear_zone_body_entered)
 
 
 func _exit_tree() -> void:
 	Services.event_bus.unsubscribe(GameEventTypes.ENEMY_TARGET_ACQUIRED, _on_aooni_target_acquired)
+	Services.event_bus.unsubscribe(GameEventTypes.CUSTOM_BLUEBERRY_FARM_CHASE_SEQUENCE, _on_blueberry_farm_chase_sequence)
+	if blueberry_farm_open_door_zone:
+		blueberry_farm_open_door_zone.body_entered.disconnect(_on_blueberry_farm_open_door_zone_body_entered)
+	if blueberry_farm_all_enemies_disappear_zone:
+		blueberry_farm_all_enemies_disappear_zone.body_entered.disconnect(_on_blueberry_farm_all_enemies_disappear_zone_body_entered)
 
 
 func _hud() -> Node:
@@ -92,3 +116,59 @@ func _on_fuwatty_disappear() -> void:
 	var hud := _hud()
 	if hud:
 		hud.show_event_text("[color=#6c6c6c]You:[/color] I think it disappeared...", false, 3.0)
+
+
+func _on_blueberry_farm_chase_sequence(event: GameEvent) -> void:
+	var player: Player = event.payload.get("body")
+	if not (player is Player):
+		return
+
+	var seq := SequenceData.create(&"blueberry_farm_chase_sequence")
+	seq.wait(randf_range(6.0, 13.0))
+	seq.custom(_on_blueberry_farm_chase_sequence_custom.bind(player))
+	seq.wait(randf_range(1.0, 10.0))
+	seq.custom(_on_blueberry_farm_chase_sequence_custom_2.bind(player))
+	Services.sequence_director.play_sequence(seq)
+
+
+func _on_blueberry_farm_chase_sequence_custom(player: Player) -> void:
+	blueberry_farm_blocker.queue_free()
+	var first_enemy: Enemy = [blueberry_farm_jellyfish, blueberry_farm_squatto].pick_random()
+	first_enemy.current_target = player
+	first_enemy.makepath()
+
+
+func _on_blueberry_farm_chase_sequence_custom_2(player: Player) -> void:
+	if blueberry_farm_jellyfish and blueberry_farm_jellyfish.current_target != player:
+		blueberry_farm_jellyfish.current_target = player
+		blueberry_farm_jellyfish.makepath()
+	if blueberry_farm_squatto and blueberry_farm_squatto.current_target != player:
+		blueberry_farm_squatto.current_target = player
+		blueberry_farm_squatto.makepath()
+
+
+func _on_blueberry_farm_open_door_zone_body_entered(body: Enemy) -> void:
+	if not body:
+		return
+	if _blueberry_farm_open_door_zone_triggered:
+		return
+	_blueberry_farm_open_door_zone_triggered = true
+	blueberry_door.open()
+
+
+func _on_blueberry_farm_all_enemies_disappear_zone_body_entered(body: Player) -> void:
+	if not body:
+		return
+	if _blueberry_farm_all_enemies_disappear_zone_triggered or not _blueberry_farm_open_door_zone_triggered:
+		return
+	_blueberry_farm_all_enemies_disappear_zone_triggered = true
+	if blueberry_farm_fuwatty:
+		blueberry_farm_fuwatty.queue_free()
+	if blueberry_farm_jellyfish:
+		blueberry_farm_jellyfish.queue_free()
+	if blueberry_farm_hundred_eyed_monster:
+		blueberry_farm_hundred_eyed_monster.queue_free()
+	if blueberry_farm_squatto:
+		blueberry_farm_squatto.queue_free()
+	if blueberry_farm_hunchback_monster:
+		blueberry_farm_hunchback_monster.queue_free()
